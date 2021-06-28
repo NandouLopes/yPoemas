cf22f5b0139b34622ddb54c461982a2dafea6896
nando_lopes <74074593+NandouLopes@users.noreply.github.com>
2021-06-28 13:19:03 -0300

    Add files via upload

---
+++ lay_2_ypo.py
@@ -0,0 +1,445 @@
+import os
+import random
+import datetime
+from random import randrange
+
+def gera_poema(nome_tema):  # abrir um script.ipo e gerar um novo texto
+    """
+    :param = script, tema
+         numero_linea = '01'  # linha
+         ideia_numero = '01'  # ideia
+         fonte_itimos = 'NA'  # fonte
+         se_randomico = 'F'   # se_random
+         total_itimos = N     # qtd_itimos
+         itimos_atual = 1     # itimos_atual
+         array_itimos = []    # array com todos os itimos da ideia na linha
+    return: novo_poema
+
+    ToDo:
+       - UpDate_Numbers() = ler *.ipo da pasta e verbetes.append(cada_verbete_novo) = done in cata_pala()
+       - usar temas.rol - criar Livro_Vivo
+       - get and hilight semente in zero.py
+       - dialeto em ciuminho
+    """
+
+    lista_header = []
+    lista_linhas = []
+    lista_finais = []
+    lista_change = []
+    lista_duplos = []
+    lista_errata = []
+    lista_unicos = []
+
+    conta_palavra = 0
+
+    pega_semente = ""
+    acha_semente = False
+    qual_semente = ""
+    # fonte_da_semente = AllTrim(Application:oMainForm:oCfg:fonte_semente)
+    fonte_da_semente = "nonono"
+
+    nome_tema = nome_tema.replace("\n", "")
+
+    try:
+        tema = abre(nome_tema)
+        for line in tema:
+            if line.startswith("*", 0, 1):  # observações e cabeçalho
+                lista_header.append(line)
+            elif line.startswith("|", 0, 1):  # ideias & itimos
+                lista_linhas.append(line)
+            else:  # <eof> + análise + build_date
+                lista_finais.append(line)
+    except UnicodeDecodeError:
+        lista_errata.append(nome_tema)
+        pass
+
+    novo_poema = []
+    # novo_poema.append(nome_tema + '\n')
+    novo_verso = ""
+    muda_linha = "00"
+    pula_linha = "no"
+
+    for line in lista_linhas:
+        alinhas = line.split("|")
+
+        if len(alinhas) == 0:
+            continue
+
+        if len(alinhas) < 2:
+            lista_errata.append(nome_tema)
+            continue
+
+        if alinhas[2] == "00":
+            pula_linha = "si"
+            lista_change.append(line)
+            continue
+
+        if len(alinhas) >= 7:
+            numero_linea = alinhas[1]
+            ideia_numero = alinhas[2]
+            fonte_itimos = alinhas[3]
+            se_randomico = alinhas[4]
+            total_itimos = int(alinhas[5])
+            itimos_atual = int(alinhas[6])
+            array_itimos = alinhas[7: len(alinhas) - 1]
+
+            for itimo in array_itimos:
+                if (
+                        fonte_itimos == fonte_da_semente
+                ):  # Linha escolhida em TFormDemo
+                    if qual_semente.upper() in itimo.upper():
+                        if not acha_semente:  # Pega apenas uma vez...
+                            qual_semente = itimo
+                            acha_semente = True
+
+            # while True # para não repetir itimos
+            tentativas = 0
+            while True:  # Seleciona próximo ítimo...
+                if (qual_semente != "") and (acha_semente):
+                    pega_semente = qual_semente.replace(
+                        qual_semente,
+                        Chr(171) + " " + qual_semente + " " + Chr(187),
+                    )  # Adiciona MARCA ao texto
+                    lista_unicos.append(
+                        qual_semente.upper()
+                    )  # adiciona à lista SEM MARCA para não repetir...
+                    acha_semente = True  # Substitui apenas uma vez...
+                    novo_verso += pega_semente + " "
+                    break  # Seleciona próximo ítimo...
+
+                if 1 != total_itimos:  # mais de hum itimo
+                    if se_randomico == "F":
+                        itimos_atual -= 1  # because matrix começa em zero
+                        if itimos_atual < 0:
+                            itimos_atual = total_itimos - 1  # because matrix começa em zero
+                    else:
+                        if total_itimos >= 1:
+                            itimos_atual = randrange(0, total_itimos - 1)
+                        else:
+                            itimos_atual = 0
+                else:
+                    itimos_atual = 0
+
+                #  if itimos_atual >= 0 and itimos_atual <= len(array_itimos) - 1:
+                if itimos_atual >= 0 and itimos_atual <= len(array_itimos):
+                    itimo_escolhido = array_itimos[itimos_atual]
+                else:
+                    print(nome_tema, " - error: ", array_itimos, itimos_atual)
+                    itimo_escolhido = "_Erro_"
+
+                #   Elimina duplicidaders óbvias...
+                temp_random = se_randomico
+                if (
+                        not itimo_escolhido.upper()
+                            in "_E_A_AS_O_OS_NO_NOS_NA_NAS_ME_DE_SE_QUE_NÃO_SO_SEM_NEM_EM_UM_UMA_POR_MEU_VE_TE_TÃO_DA_SER_TER_PRA_PARA_QUANDO_..._._,_:_!_?"
+                ):
+                    if (
+                            itimo_escolhido.upper() not in lista_unicos
+                    ):  # verifica se Itimo ainda não foi usado...
+                        lista_unicos.append(itimo_escolhido.upper())
+                        break
+                    else:  # Já foi usado... Busca Outro...
+                        tentativas += 1
+                        if (
+                                tentativas > total_itimos
+                        ):  # Tentativas > que total de ítimos: pega o próximo sequencial
+                            if temp_random == "T":
+                                tentativas = 0  # Da Capo
+                                temp_random = "F"
+                            else:
+                                lista_unicos.append(itimo_escolhido.upper())
+                                lista_duplos.append(itimo_escolhido.upper())
+                                break
+
+                        if itimo_escolhido in lista_duplos:
+                            if len(itimo_escolhido) > 3:
+                                # print(itimo_escolhido, " ---> repetido")
+                                continue
+
+                        if tentativas > 50:
+                            break
+                else:
+                    break
+
+            # verifica se é nova linha no script
+            if numero_linea != muda_linha:  # mudou de linha
+                novo_poema.append(acerto_final(novo_verso, nome_tema))
+                novo_verso = ""
+                muda_linha = numero_linea
+
+            novo_verso += itimo_escolhido + " "
+
+            if "si" == pula_linha:
+                novo_poema.append("\n")
+                pula_linha = "no"
+
+            new_line = (
+                    "|"
+                    + numero_linea
+                    + "|"
+                    + ideia_numero
+                    + "|"
+                    + fonte_itimos
+                    + "|"
+            )
+
+            if total_itimos != len(array_itimos):  # just in case...
+                total_itimos = len(array_itimos)
+            if itimos_atual < 1:  # sequencial = -1
+                if total_itimos == 1:
+                    itimos_atual = 1
+                else:
+                    itimos_atual = total_itimos
+            if se_randomico == "T":
+                new_line += "T"
+            else:
+                new_line += "F"
+            new_line += "|" + str(total_itimos) + "|" + str(itimos_atual)
+
+            for v in alinhas[7: len(alinhas) - 1]:
+                new_line += "|" + v
+            new_line += "|\n"
+            lista_change.append(new_line)
+    # end for... lista_linhas
+
+    novo_poema.append(acerto_final(novo_verso, nome_tema))
+    catapala = conta_palas(novo_poema)
+    catapala = str(catapala)
+
+    #    analise = get_numbers(nome_tema)
+    #    analise.strip(' ')
+    #    analise = analise.replace(nome_tema + ' = ', '')  # apenas os números da análise combinatória
+    #    novo_poema.append(analise)
+
+    if len(lista_errata) > 0:
+        print(lista_errata)
+    else:
+        # reconstrói o script com novas posições
+        novo_script = os.path.join("./data/", nome_tema + ".ipo")
+        with open(novo_script, "w") as file:
+            for linha in lista_header:
+                file.write(linha)
+        
+            for linha in lista_change:
+                file.write(linha)
+        
+            for linha in lista_finais:
+                if 'CATAPALA' in str(linha):
+                    del (linha)
+        
+            if "CATAPALA" not in str(lista_finais):  # se não gravou...
+                lista_finais.insert(1, "CATAPALA = " + catapala + "\n")
+        
+            for linha in lista_finais:
+                if "CATAPALA" in str(linha):
+                    linha = "CATAPALA = " + catapala + "\n"
+                file.write(linha)
+        file.close()
+        
+    return novo_poema
+
+
+def acerto_final(texto, nome_tema):
+    if " ." in texto:
+        texto = texto.replace(" .", ".")
+    if " ," in texto:
+        texto = texto.replace(" ,", ",")
+    if " ?" in texto:
+        texto = texto.replace(" ?", "?")
+    if " !" in texto:
+        texto = texto.replace(" !", "!")
+    if " :" in texto:
+        texto = texto.replace(" :", ":")
+    if " ..." in texto:
+        texto = texto.replace(" ...", "...")
+    if " -" in texto:
+        texto = texto.replace(" -", "-")
+    if "- " in texto:
+        texto = texto.replace("- ", "-")
+    if " #" in texto:  # apenas usado em Bula para concatenar 3 palavras
+        texto = texto.replace(" #", "")
+    if "#" in texto:
+        texto = texto.replace("#", "")
+    if "< pCity >" in texto:
+        texto = texto.replace("< pCity >", load_cidade_fato())
+    if "< pCidadeOficio >" in texto:
+        texto = texto.replace("< pCidadeOficio >", fala_cidade())
+    if "< gCelcius >" in texto:
+        texto = texto.replace("< gCelcius >", fala_celsius())
+    if "< pUmido >" in texto:
+        texto = texto.replace("< pUmido >", fala_umidade())
+    if "< dNormas >" in texto:
+        texto = texto.replace("< dNormas >", fala_norma_abnp())
+    if "< dPublic >" in texto:
+        hoje = datetime.datetime.now().date()
+        rand = randrange(0, hoje.year * 30)
+        hoje = hoje - datetime.timedelta(days=rand)
+        texto = texto.replace("< dPublic >", fala_data(hoje))
+    if "< dOficio >" in texto:
+        hoje = datetime.datetime.now().date()
+        rand = randrange(0, hoje.year * 30)
+        hoje = hoje + datetime.timedelta(days=rand)
+        texto = texto.replace("< dOficio >", fala_data(hoje))
+    if "< nAnalise >" in texto:
+        anali = get_numbers(nome_tema)
+        anali.strip()
+        anali = anali.replace(
+            nome_tema + " = ", ""
+        )  # apenas os números da análise combinatória
+        texto = texto.replace("< nAnalise >", anali)
+
+    return texto
+
+
+def get_numbers(nome_tema):
+    """
+    :return: análise combinatória do tema
+    """
+    ana_con = "not in list"
+    numbers = []
+
+    lista = []
+    with open(os.path.join("./data/index.num"), "r") as file:
+        for line in file:
+            lista.append(line)
+        file.close()
+
+    for line in lista:
+        numbers.append(line.strip())
+        if line.startswith(nome_tema):
+            ana_con = line
+    return ana_con
+
+
+def load_cidade_fato():
+    """
+    :return: alguma cidade do arquivo fatos_cidades.txt
+    """
+    cidades = []
+    with open(os.path.join("./data/Fatos_Cidades.Txt"), "r") as file:
+        for line in file:
+            cidades.append(line)
+        file.close()
+
+    x = randrange(0, len(cidades))
+    city = cidades[x]
+    city = city.replace("\n", "")
+    return city
+
+
+def fala_cidade():
+    """
+    :return: alguma cidade do arquivo cidade_país.txt
+    """
+    cidades = []
+    # cidade_pais.txt ## não conseguiu abrir o codec !!!
+    with open(os.path.join("./data/fatos_cidades.txt"), "r") as file:
+        for line in file:
+            cidades.append(line)
+        file.close()
+
+    x = randrange(0, len(cidades))
+    city = cidades[x]
+    city = city.replace("\n", "")
+
+    return city
+
+
+def fala_celsius():
+    """
+    :return: temperatura randômica entre 1 e 50 graus celcius - Meteoro
+    """
+    ini = randrange(1, 50)
+    fim = randrange(1, 50)
+    if ini > fim:
+        tmp = ini
+        ini = fim
+        fim = tmp
+    else:
+        ini -= 1
+    return str(ini) + "º e " + str(fim) + "º"
+
+
+def fala_umidade():
+    """
+    :return: umidade randômica entre 1 e 99% - Meteoro
+    """
+    ini = randrange(1, 99)
+    return str(ini) + "%"
+
+
+def fala_data(dref):
+    """
+    :param data de referência
+    :return: data genérica: dia + mês_extenso + ano
+    """
+    meses = [
+        "Janeiro",
+        "Fevereiro",
+        "Março",
+        "Abril",
+        "Maio",
+        "Junho",
+        "Julho",
+        "Agosto",
+        "Setembro",
+        "Outubro",
+        "Novembro",
+        "Dezembro",
+    ]
+
+    dia = dref.day
+    mes = dref.month
+    if mes > 0 and mes < 13:
+        mes -= 1
+    else:
+        mes = 5
+
+    mestxt = meses[mes]
+    ano = dref.year
+    return str(dia) + " de " + str(mestxt) + " de " + str(ano)
+
+
+def fala_norma_abnp():
+    """
+    :return: data randômicamente 'anterior' à data atual
+    """
+    hoje = datetime.datetime.now().date()
+    rand = randrange(0, hoje.year * 30)
+    hoje = hoje - datetime.timedelta(days=rand)
+    return str(hoje.day) + "/" + str(hoje.year)
+
+
+def abre(nome_do_tema):
+    """
+    :param nome_do_tema
+    :return: lista do arquivo
+    """
+
+    full_name = os.path.join("./data/", nome_do_tema) + ".ipo"
+    lista = []
+    with open(full_name, encoding="ansi") as file:
+        for line in file:
+            lista.append(line)
+        file.close()
+
+    return lista
+
+
+def conta_palas(frase):
+    """
+    :param frase
+    :return: quantidade de palavras na "frase" = todas as linhas do poema gerado
+    """
+
+    # palavras = frase.split(" ")  # separa palavras
+    texto = ""
+    somas = 0
+    for p in frase:
+        texto += p
+
+    for p in texto:
+        if p == " ":
+            somas += 1
+
+    return somas
---
+++ ypo.py
@@ -0,0 +1,408 @@
+"""
+
+an App that randomly gathers words and phrases
+from specific databases and organize them
+into thousands of new poems or poetic texts.
+
+LYPO == Last YPOema created from curr_ypoema
+TYPO == Translated Ypoema from LYPO
+user_IP == the owner of LYPO and TYPO
+
+[todo] -  criar (def is_online) antes do Translator
+
+"""
+
+import os
+import random
+import streamlit as st
+
+# Project Modules
+import SessionState
+from lay_2_ypo import gera_poema
+
+# Translators
+from deep_translator import GoogleTranslator
+
+# user_IP: to create LYPO and TYPO for each hostname
+import socket
+
+hostname = socket.gethostname()
+user_IP = socket.gethostbyname(hostname)
+
+# ⚛	:atom_symbol:
+# ✔ :check_mark:
+# Ⓜ	 :circled_M:
+# ☄ :comet:
+# ‼	 :double_exclamation_mark
+# ⚙	 :gear:
+# ♾	 :infinity:
+# ❇  :sparkle:
+# ☀ :sun:
+# ¿  : spanish question
+
+st.set_page_config(
+    page_title = 'yPoemas - a "machina" de fazer Poesia',
+    page_icon = ":star:",
+    layout = "centered",
+    initial_sidebar_state = "auto")
+
+# hide Streamlit Menu
+st.markdown(""" <style>
+MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style> """, unsafe_allow_html = True)
+
+# change padding between components
+padding = 0  # all set to zero
+st.markdown(
+    f""" <style>
+    .reportview-container .main .block-container{{
+        padding-top: {1.8}rem;
+        padding-right: {.2}rem;
+        padding-left: {.2}rem;
+        padding-bottom: {.2}rem;
+    }} </style> """, unsafe_allow_html = True)
+
+# Initialize SessionState
+session_state = SessionState.get(
+    take = 0, book = "livro vivo", lang = "pt")
+
+# bof: loaders
+@st.cache(allow_output_mutation = True)
+def load_file(file):  # Open files for about's
+    if file == "LICENSE.md":  ## ./ypo
+        with open("LICENSE.md", encoding = "utf8") as f:
+            file_text = f.read()
+    else:  ## ./data
+        with open(os.path.join("./data/" + file), encoding = "utf8") as f:
+            file_text = f.read()
+
+    if (file not in "THEMES.md_INDEX.md"):# >5000 chars not alowed in translator
+        if not ".rol" in file:  # and...don't want to translate original titles.
+            file_text = translate(file_text)
+
+    return file_text
+
+
+@st.cache(allow_output_mutation = True)
+def load_tems(book):  # List of yPoemas' themes inside a Book
+    temas_list = []
+    full_name = os.path.join("./data/", book) + ".rol"
+    with open(full_name, encoding = "ansi") as file:
+        for line in file:
+            temas_list.append(line)
+    return temas_list
+
+
+@st.cache(allow_output_mutation = True)
+def load_index():  # Load indexes for all themes
+    index_list = []
+    with open(os.path.join("./data/index.num"), encoding = "utf-8") as lista:
+        for line in lista:
+            index_list.append(line)
+    return index_list
+
+
+def load_lypo():  # load last yPoema & clean translator returned text
+    lypo_text = ""
+    lypo_user = "LYPO_" + user_IP
+    with open(os.path.join("./temp/" + lypo_user), encoding = "utf-8") as script:
+        for line in script:
+            line = line.strip()
+            line = line.replace("< br>", "<br>")
+            line = line.replace("<br >", "")
+            line = line.replace(" br>", "")
+            line = line.replace(" >", "")
+            lypo_text += line + "<br>"
+    return lypo_text
+
+
+def load_typo():  # load translated yPoema & clean translator returned text
+    typo_text = ""
+    typo_user = "TYPO_" + user_IP
+    with open(os.path.join("./temp/" + typo_user), encoding = "utf-8") as script:
+        for line in script:
+            line = line.strip()
+            line = line.replace("< br>", "<br>")
+            line = line.replace("<br >", "")
+            line = line.replace(" br>", "")
+            line = line.replace(" >", "")
+            typo_text += line + "<br>"
+    return typo_text
+
+
+def load_poema():  # generate new yPoema & save a copy of last generated in LYPO
+    nome_tema = temas_list[session_state.take]
+    script = gera_poema(nome_tema)
+
+    novo_ypoema = ""
+    lypo_user = "LYPO_" + user_IP
+
+    with open(os.path.join("./temp/" + lypo_user), "w", encoding = "utf-8") as save_lypo:
+        # save_lypo.write(nome_tema + '\n')  ## title of yPoema in first line
+        for line in script:
+            if line == "\n":
+                save_lypo.write("\n")
+                novo_ypoema += "<br>"
+            else:
+                save_lypo.write(line + "\n")
+                novo_ypoema += line + "<br>"
+    save_lypo.close()
+    return novo_ypoema
+# eof: loaders
+
+
+# bof: functions
+def last_next(updn):  # handle last, random and next theme
+    last_tema = len(temas_list) - 1
+    if updn == ">":
+        session_state.take += 1
+        if session_state.take > last_tema:
+            session_state.take = 0
+    elif updn == "<":
+        session_state.take -= 1
+        if session_state.take < 0:
+            session_state.take = last_tema
+    else:
+        session_state.take = random.randrange(0, last_tema, 1)
+    return session_state.take
+
+
+def say_numbers(index):  # search index title in index.num
+    indexes = load_index()
+    analise = "nonono ..."
+    this = temas_list[index].strip()
+    for item in indexes:
+        if item.startswith(this, 0, len(this)):
+            analise = "#️ " + item
+    return analise
+
+
+def translate(input_text):
+    try:
+        output_text = GoogleTranslator(source = "auto", target = session_state.lang).translate(
+            text = input_text
+        )
+    except IOError as exc:
+        raise RuntimeError(
+            "oops... Google Translator não está repondendo... Offline?"
+        ) from exc
+    return output_text
+# eof: functions
+
+
+# bof: pages
+def main():
+    pages = {
+        "yPoemas": page_home,
+        "books": page_books,
+        "about": page_abouts,
+        "license": page_license,}
+        
+    page = st.sidebar.radio("", tuple(pages.keys()))
+    pages[page]()
+    st.sidebar.info(load_file("PROJETO.md"))
+    st.sidebar.state = True
+
+
+def page_books():  # available books
+    st.write("")
+    st.sidebar.image('./img_books.jpg')
+    books_expander = st.beta_expander("", True)
+    with books_expander:
+        books_list = [
+            "livro vivo",
+            "poemas",
+            "ensaios",
+            "jocosos",
+            "variações",
+            "metalingua",
+            "outros autores",
+            "todos os temas",
+            "todos os signos",
+            "signos_fem",
+            "signos_mas",
+        ]
+        this = books_expander.radio("", books_list)
+        session_state.take = 0
+        session_state.book = this
+        st.info(translate("escolha um livro e click em yPoemas para voltar à leitura..."))
+        
+    list_book = ""
+    temas_list = load_tems(session_state.book)
+    for line in temas_list:
+        list_book += line
+    show_expander = st.beta_expander("", True)
+    with show_expander:
+        st.write(list_book)
+
+
+def page_abouts():
+    st.write("")
+    st.sidebar.image('./img_about.jpg')
+
+    about_expander = st.beta_expander("", True)
+    with about_expander:
+        abouts_list = [
+            "machina",
+            "bibliografia",
+            "traduttore",
+            "outros",
+        ]
+        this = about_expander.radio("", abouts_list)
+
+    show_expander = st.beta_expander("", True)
+    with show_expander:
+        st.subheader(load_file(this + ".md"))
+
+
+def page_license():
+    st.write("")
+    st.sidebar.image('./img_license.jpg')
+    licence_expander = st.beta_expander("", True)
+    with licence_expander:
+        st.subheader(load_file("LICENSE.md"))
+
+
+last_idioma = session_state.lang
+temas_list = load_tems(session_state.book)
+
+
+def page_home():
+    st.sidebar.image('./img_home.jpg')
+    i1, i2, i3, i4, i5, i6, i7, last, rand, nest, numb, manu = st.beta_columns(
+        [.9, .9, .85, .85, .9, .9, 1.4, .9, 1, 1.3, 0.9, 0.8]  # what a mess...
+    )
+    i1 = i1.button("pt", help = "Português")
+    i2 = i2.button("es", help = "Español")
+    i3 = i3.button("it", help = "Italiano")
+    i4 = i4.button("fr", help = "Français")
+    i5 = i5.button("en", help = "English")
+    i6 = i6.button("de", help = "Deutsche")
+    i7 = i7.button("ca", help = "Català")
+
+    if i1:
+        session_state.lang = "pt"
+    elif i2:
+        session_state.lang = "es"
+    elif i3:
+        session_state.lang = "it"
+    elif i4:
+        session_state.lang = "fr"
+    elif i5:
+        session_state.lang = "en"
+    elif i6:
+        session_state.lang = "de"
+    elif i7:
+        session_state.lang = "ca"
+
+    if session_state.lang == "pt":
+        last = last.button("◀", help = "anterior")
+        rand = rand.button("❇", help = "escolhe tema ao acaso")
+        nest = nest.button("▶", help = "próximo")
+        bar_help = "barra de temas"
+    elif session_state.lang == "es":
+        last = last.button("◀", help = "anterior")
+        rand = rand.button("❇", help = "elige un tema al azar")
+        nest = nest.button("▶", help = "próximo")
+        bar_help = "barra de temas"
+    elif session_state.lang == "it":
+        last = last.button("◀", help = "precedente")
+        rand = rand.button("❇", help = "scegliere un tema a caso")
+        nest = nest.button("▶", help = "prossimo")
+        bar_help = "barra dei temi"
+    elif session_state.lang == "fr":
+        last = last.button("◀", help = "précédent")
+        rand = rand.button("❇", help = "choisir le thème au hasard")
+        nest = nest.button("▶", help = "prochain")
+        bar_help = "barre à thèmes"
+    elif session_state.lang == "en":
+        last = last.button("◀", help = "last")
+        rand = rand.button("❇", help = "pick theme at random")
+        nest = nest.button("▶", help = "next")
+        bar_help = "themes bar"
+    elif session_state.lang == "ca":
+        last = last.button("◀", help = "anterior")
+        rand = rand.button("❇", help = "tria un tema a l'atzar")
+        nest = nest.button("▶", help = "següent")
+        bar_help = "barra de temes"
+    elif session_state.lang == "de":
+        last = last.button("◀", help = "letzte")
+        rand = rand.button("❇", help = "ändert das thema zufällig")
+        nest = nest.button("▶", help = "nächster")
+        bar_help = "Themenleiste"
+    else:  # for new languages...
+        last = last.button("◀", help = "last")
+        rand = rand.button("❇", help = "pick theme at random")
+        nest = nest.button("▶", help = "next")
+        bar_help = "barra de temas"
+
+    if last:
+        last_next("<")
+    if rand:
+        last_next("*")
+    if nest:
+        last_next(">")
+
+    numb = numb.button("‼", help = say_numbers(session_state.take))
+    manu = manu.button("?", help = "help !!!")
+    
+    lnew = True
+    if manu:
+        st.subheader(load_file("MANUAL.md"))
+        lnew = False
+
+    if numb:
+        st.subheader(load_file("INDEX.md"))
+        lnew = False
+
+    if lnew:
+        info = (
+            session_state.lang
+            + " ( "
+            + session_state.book
+            + " ) ( "
+            + str(session_state.take + 1)
+            + "/"
+            + str(len(temas_list))
+            + " )"
+        )
+
+        options = list(range(len(temas_list)))
+        session_state.take = st.selectbox(
+            "",
+            options,
+            format_func = lambda x: temas_list[x],
+            index=session_state.take,
+            help = bar_help,
+        )
+
+        poemas_expander = st.beta_expander(
+            info, expanded = True
+        )
+        with poemas_expander:
+            if session_state.lang != last_idioma:
+                curr_ypoema = load_lypo()
+            else:
+                if lnew:
+                    curr_ypoema = load_poema()
+                    curr_ypoema = load_lypo()
+                else:
+                    curr_ypoema = load_lypo()
+
+            if session_state.lang != "pt":  # translate if idioma <> pt
+                curr_ypoema = translate(curr_ypoema)
+                typo_user = "TYPO_" + user_IP
+                with open(
+                    os.path.join("./temp/" + typo_user), "w", encoding = "utf-8"
+                ) as save_typo:
+                    save_typo.write(curr_ypoema)
+                    save_typo.close()
+                curr_ypoema = load_typo()  # to normalize line breaks in text
+
+            st.markdown(curr_ypoema, unsafe_allow_html = True)  # finally... write it
+# eof: pages
+
+
+if __name__ == "__main__":
+    main()
