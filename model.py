b0670da180aa10119da9e8cab4195b4768eb4457
nando_lopes <lopes.fernando@hotmail.com>
2022-05-23 10:39:44 -0300

    Update model.py

--- model.py
+++ model.py
@@ -10,7 +10,7 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+# from mpl_toolkits.mplot3d import Axes3D
 
 def load_number():  # load indexes numbers para os temas
     lista = []
