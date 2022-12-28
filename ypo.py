53b53fe888a89cc1e379ab9c27fba0ad9b9b0872
nando_lopes <lopes.fernando@hotmail.com>
2022-12-28 08:08:17 -0300

    Update ypo.py

--- ypo.py
+++ ypo.py
@@ -743,6 +743,7 @@ if st.session_state.visy:  # check visitor once
     st.session_state.mini = random.randrange(0, maxy_mini)
 
     st.success(translate("bem vindo à **máquina de fazer Poesia...**"))
+    st.session_state.draw = True
     st.session_state.visy = False
 
 
