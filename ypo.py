ba5eda11047bee76bbc9afa9a43e7756bcb5707d
nando_lopes <lopes.fernando@hotmail.com>
2023-05-12 16:27:54 -0300

    Update ypo.py

--- ypo.py
+++ ypo.py
@@ -15,7 +15,7 @@ Não vivo no meu tempo.
 
 º¤ø,¸¸,ø¤º°`°º¤ø,¸¸,ø¤º°`°ºº¤ø,¸¸,ø¤º°`°º¤ø,¸¸,ø¤º°`°ºº¤ø,¸¸,ø¤º°`°º¤ø,¸¸,ø¤º°
 
-ツpoemas
+ツpoemas : new deploy
 
 AlfaBetaAção == C:\WINDOWS\new.ini
 config.toml  == C:\Users\dkvece\.streamlit
